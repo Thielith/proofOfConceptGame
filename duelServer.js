8e9efe9a5aa3e17b1ad4a3543f118ce8ff750fbe
Ayden <aydensgame@gmail.com>
2018-01-22 09:35:12 -0800

    Function for sending data

--- duelServer.js
+++ duelServer.js
@@ -1,13 +1,20 @@
-
+var mysql = require('mysql'); 
 var io = require('socket.io').listen(33336);
 const {exec} = require('child_process');
 
+var con = mysql.createConnection({
+	host: "localhost"
+	user: "root",
+	password: "p2950"
+	database: "felix_database"
+})
  /*
   * SOCKET SETUP
   */
 io.sockets.on('connection', function (socket) {
 	var clientIp = socket.request.connection.remoteAddress;
 	console.log("Someone From " + clientIp + " Connected")
+	
 	//Edit-Update Database
 	socket.on('loadGuys', function (info) {		// 'cmd' is arbitrary
 		var sendLine = ""
@@ -19,8 +26,13 @@ io.sockets.on('connection', function (socket) {
 		exec(e);
 	});
 	
+	//Update Client Info
 	socket.on('sendData', function(){
-		
+		if (err) throw err;
+		con.quert("SELECT * FROM player;", function(result){
+			if (err) throw err;
+			console.log(result)
+		})
 	})
 	
 	socket.on('disconnect', function(){
